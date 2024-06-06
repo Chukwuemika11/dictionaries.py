f3929ae5b924ded96c63ad498ed8573453b9f286
Chukwuemika11 <117942512+Chukwuemika11@users.noreply.github.com>
2024-06-06 23:54:55 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+names = {
+    "ada" : "adanna",
+    "ada1" : "adanurn",
+    "ada2" : "adajbxd",
+    "ada3" : "adajbx",
+    "ada4" : "adajhbxr",
+}
+
+
+print(f"this are my names {names}")
+
+# dictionary in python programming language
